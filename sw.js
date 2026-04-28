01af152bee90fbab201fe3ca4994ec2d04921fea
gringono <kobe135@onlinehome.de>
2026-04-28 17:35:00 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE = 'inventur-v1';
+const ASSETS = [
+  '/inventur/inventur.html',
+  '/inventur/manifest.json',
+  '/inventur/icon-192.png',
+  '/inventur/icon-512.png',
+  'https://cdnjs.cloudflare.com/ajax/libs/jsQR/1.4.0/jsQR.min.js'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).catch(() => caches.match('/inventur/inventur.html')))
+  );
+});
